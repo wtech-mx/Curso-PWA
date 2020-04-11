6ff18c38bc340e55680100ea3b53fa0811da95c3
W-Tech-mx <52049354+GOLDBN@users.noreply.github.com>
2020-04-11 14:14:46 -0500

    updatre changes

--- curso-pwa-avanzado/02-service-worker/js/app.js
+++
@@ -1,7 +0,0 @@
-//if ('serviceWorker' in navigator) {
-  //  console.log('podemos usarlo')
-//}
-//CONFIRMAR SI PODEMOS USAR EL SW
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js')
-}
